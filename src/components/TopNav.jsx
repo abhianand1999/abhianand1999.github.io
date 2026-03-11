1fa77613ab9ddefa4bd6ce31bea37d0704bcca02
Abhi Anand <abhinavanand@berkeley.edu>
2026-03-10 23:04:09 -0500

    Saving files

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TopNav() {
+  return (
+    <nav className="top-nav">
+      <a href="#about">About Me</a>
+      <a href="#experience">Professional Experience</a>
+      <a href="#interests">Interests</a>
+    </nav>
+  );
+}
+
+export default TopNav;
