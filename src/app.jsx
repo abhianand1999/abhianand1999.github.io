01148f0acea7b3e8114615d1e7caa16be9f19d0b
Abhi Anand <abhinavanand@berkeley.edu>
2026-03-11 19:36:28 -0500

    Changing router

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';                                                                                                                                 
 import About from './components/About';
 import Experience from './components/Experience';
 import Interests from './components/Interests';
